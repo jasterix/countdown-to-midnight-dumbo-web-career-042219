3b0ca0c173c49e8902d742e3c3215ddb84d3f8b7
Jasterix C <loune.calixte@gmail.com>
2019-04-03 18:41:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,15 @@ def countdown(integer)
   while integer > 0 
     integer-=1
     puts integer
-    sleep 1
   end
     "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
-  
+      sleep 1
+        while integer > 0 
+    integer-=1
+    puts integer
+  end
+    "HAPPY NEW YEAR!"
+end
